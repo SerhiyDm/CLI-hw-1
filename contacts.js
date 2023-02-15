80c9295522616d68ff80552fa5e7ee30ef986119
serhii_dem <realsergeiii0@gmail.com>
2023-02-15 22:02:51 +0200

    correction

--- contacts.js
+++ contacts.js
@@ -1,10 +1,10 @@
 const { v4: uuidv4 } = require("uuid");
 const path = require("path");
 const file = require("fs").promises;
-const contactsPath = path.resolve("db/contacts.json");
+const contactsPath = path.resolve(__dirname, "db", "contacts.json");
 
 async function listContacts() {
-  let data = await file.readFile(contactsPath, "utf-8");
+  let data = await file.readFile(contactsPath);
   data = JSON.parse(data);
   return data;
 }
@@ -12,7 +12,7 @@ async function listContacts() {
 async function getContactById(contactId) {
   let data = await listContacts();
   data = data.find((el) => el.id === contactId.toString());
-  return data ? data : false;
+  return data ? data : null;
 }
 async function removeContact(contactId) {
   let data = await listContacts();
@@ -20,7 +20,7 @@ async function removeContact(contactId) {
     return false;
   }
   data = data.filter((el) => el.id !== contactId.toString());
-  await file.writeFile(contactsPath, JSON.stringify(data));
+  await file.writeFile(contactsPath, JSON.stringify(data, null, 2));
   return "Сontact deleted successfully!";
 }
 
@@ -28,7 +28,7 @@ async function addContact(name, email, phone) {
   const contact = { name, email, phone, id: uuidv4() };
   let data = await listContacts();
   data.unshift(contact);
-  await file.writeFile(contactsPath, JSON.stringify(data));
+  await file.writeFile(contactsPath, JSON.stringify(data, null, 2));
   return contact;
 }
 
